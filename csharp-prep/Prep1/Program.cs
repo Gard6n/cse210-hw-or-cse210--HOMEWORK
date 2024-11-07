52c5074d0679fe47563cfba29e5a831ca749c18d
Tyler Scasella <41729165+Gard6n@users.noreply.github.com>
2024-11-06 19:43:10 -0700

    Finish with homework what was asked was delivered!

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello LOL World!");
+       string first; 
+       string last; 
+
+       Console.WriteLine("What is your first Name? ");
+        first = Console.ReadLine();
+        Console.WriteLine("What is your Last name?");
+        last = Console.ReadLine();
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
 }
