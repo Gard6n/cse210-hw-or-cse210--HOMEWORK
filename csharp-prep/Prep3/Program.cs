32606fe3ef9902a42019f8bee335b7f0f3cd46c6
Tyler Scasella <41729165+Gard6n@users.noreply.github.com>
2024-11-06 22:00:24 -0700

    Prep3 is DOOOOOONE!!

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+       int Guess = 0;
+       string guess = "";
+       int counter = 0;
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 11);
+
+        Console.WriteLine("Guess the number game!!!!");
+        Console.WriteLine("Input a number to Guess");
+
+        while (!(number == Guess)){
+
+            guess = Console.ReadLine();
+            Guess = int.Parse(guess);
+
+            if (Guess < number){
+                Console.WriteLine("Guess Higher!");
+                counter++;
+            }
+
+            if (Guess > number){
+                Console.WriteLine("Guess Lower!");
+                counter++;
+            }
+        }
+        
+        counter++;
+       Console.WriteLine($"YOU WIN, U GUESSED {counter} TIMES!");
     }
-}
+}  
