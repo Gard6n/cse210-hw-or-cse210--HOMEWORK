6729990ff3024340e80c95b7d3516a292256589c
Tyler Scasella <41729165+Gard6n@users.noreply.github.com>
2024-11-07 12:15:24 -0700

    Prep5 is Done, the code does what is was asked to do

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+       //Display opening
+        static void DisplayMes(){
+            Console.WriteLine("Welcome to the Program!");
+        }
+        //User Name
+        static string Userinput(){
+            Console.WriteLine("Please enter your name: ");
+            string name = Console.ReadLine();
+            return name;
+        }
+        //User Favorite Number
+        static int UserNum(){
+            Console.WriteLine("Please Enter your favoritee Number");
+            string faa = Console.ReadLine();
+            int fav = int.Parse(faa);
+        //Squares the number
+            int Fav = fav*fav;
+            return Fav;
+        }
+        //Results
+        static void Results(string na, int nee){
+            Console.WriteLine($"{na}, the square of your is {nee}");
+        }
+        //Calling the methods
+        DisplayMes();
+        Results(Userinput(),UserNum());
     }
 }
