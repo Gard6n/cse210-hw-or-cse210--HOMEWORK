d93cc3d07037be68ac1e562a0f8f69cc300fe459
Tyler Scasella <41729165+Gard6n@users.noreply.github.com>
2024-11-06 20:23:44 -0700

    Prep2 is DONE

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -2,8 +2,35 @@ using System;
 
 class Program
 {
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+       string Grade;
+       string grade ="";
+
+       Console.WriteLine("Enter a Grade Percentage ");
+       Grade = Console.ReadLine();
+       int num = int.Parse(Grade);
+
+       if (num >= 90 ){
+        grade = "A";
+       };
+
+       if (num >= 80 && num < 90){
+        grade = "B";
+       }
+
+       if (num >= 70 && num < 80){
+        grade = "C";
+       }
+
+       if (num >= 60 && num < 70){
+        grade = "D";
+       } 
+
+       if (num < 60){
+        grade = "F";
+       }
+         Console.WriteLine($"Your Grade is {grade}");
     }
 }
