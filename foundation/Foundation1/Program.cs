caffdf1b555b3ecf3e764bb20c417b8b3fa1bd64
Tyler Scasella <41729165+Gard6n@users.noreply.github.com>
2024-11-15 22:45:40 +0000

    Adds menus for main program

--- foundation/Foundation1/Program.cs
+++ foundation/Foundation1/Program.cs
@@ -5,7 +5,17 @@ class Program
     static void Main(string[] args)
     {
         
-        
+        Console.WriteLine("Welcome to the Journal Program!");
+        Console.WriteLine("Please selcet one of the following choices: ");
+
+        Console.WriteLine("1.Write");
+        Console.WriteLine("2.Display");
+        Console.WriteLine("Load");
+        Console.WriteLine("Save");
+        Console.WriteLine("Quit");
+        Console.WriteLine("What would you like to do? ");
+        string input = Console.ReadLine();
+
 
 
 
