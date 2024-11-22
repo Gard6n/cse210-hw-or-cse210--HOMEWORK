fc74063c2373ca31c772788cdce013a290dafd07
Tyler Scasella <41729165+Gard6n@users.noreply.github.com>
2024-11-21 18:02:53 -0700

    %70 done with program

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,45 @@
+﻿namespace Learning03;
+
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public void Fraction1()
+    {
+        _top = 1;
+        _bottom = 1;
+        Console.WriteLine($"{_top}/{_bottom}\n");
+    }
+
+    public void Fraction2()
+    {
+        Console.WriteLine($"{_top}\n");
+    }
+
+    public void Fraction3()
+    {
+        Console.WriteLine($"{_top}/{_bottom}\n");
+    }
+
+    public int GetTop()
+    {
+        return _top;
+    }
+
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+    
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using Learning03;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction math = new();
+    
+        Console.WriteLine("Enter in a numerator\n");
+        int Numerator = int.Parse(Console.ReadLine());
+        math.SetTop(Numerator);
+        
+        Console.WriteLine("Enter in a denominator\n");
+        int Denominator = int.Parse(Console.ReadLine());
+        math.SetBottom(Denominator);
+        
+        math.Fraction1();
+        math.Fraction2();
+        math.Fraction3();
+        
+
     }
 }
